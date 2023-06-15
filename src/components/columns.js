c743188565302ab03e1ec117251d8fd4155e7d84
agnesem670 <agnesem670@gmail.com>
2023-06-15 19:56:28 +0200

    Formatting

--- src/components/columns.js
+++ src/components/columns.js
@@ -1,3 +1,5 @@
+import {format} from 'date-fns'
+
 export const COLUMNS = [
     {
         Header: 'Id',
@@ -17,7 +19,8 @@ export const COLUMNS = [
     {
         Header: 'Date Of Birth',
         Footer: 'Date Of Birth',
-        accessor: 'date_of_birth'
+        accessor: 'date_of_birth',
+        Cell: ({value}) => {return format(new Date (value), 'dd/MM/yyyy')} 
     },
     {
         Header: 'Country',
