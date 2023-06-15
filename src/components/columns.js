f2dc4222ec892a04153cfc2524a2381481e69ca0
agnesem670 <agnesem670@gmail.com>
2023-06-15 22:21:42 +0200

    Disable filter, default column, AsyncDebounce

--- src/components/columns.js
+++ src/components/columns.js
@@ -6,38 +6,33 @@ export const COLUMNS = [
         Header: 'Id',
         Footer: 'Id',
         accessor: 'id',
-        Filter: ColumnFilter
+        disableFilters: true
     },
     {
         Header: 'First Name',
         Footer: 'First Name',
         accessor: 'first_name',
-        Filter: ColumnFilter
     },
     {
         Header: 'Last Name',
         Footer: 'Last Name',
         accessor: 'last_name',
-        Filter: ColumnFilter
     },
     {
         Header: 'Date Of Birth',
         Footer: 'Date Of Birth',
         accessor: 'date_of_birth',
         Cell: ({value}) => {return format(new Date (value), 'dd/MM/yyyy')},
-        Filter: ColumnFilter 
     },
     {
         Header: 'Country',
         Footer: 'Country',
         accessor: 'country',
-        Filter: ColumnFilter
     },
     {
         Header: 'Phone Number',
         Footer: 'Phone Number',
         accessor: 'phone',
-        Filter: ColumnFilter
     }
 ]
 
