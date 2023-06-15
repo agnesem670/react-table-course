471db52420456a597b776b4a6e06aecd2cff9486
agnesem670 <agnesem670@gmail.com>
2023-06-15 20:13:14 +0200

    Global Filtering

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import BasicTable from './components/BasicTable';
 import SortingTable from './components/SortingTable';
+import FilteringTable from './components/FilteringTable';
 
 function App() {
   return (
     <div className="App">
-      <SortingTable />
+      <FilteringTable />
+      {/* <SortingTable /> */}
       {/* <BasicTable /> */}
     </div>
   );
