d9c717bf0432c6b6d437e668149420007d9fa990
agnesem670 <agnesem670@gmail.com>
2023-06-15 23:51:39 +0200

    Pagination

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import BasicTable from './components/BasicTable';
 import SortingTable from './components/SortingTable';
 import FilteringTable from './components/FilteringTable';
+import PaginationTable from './components/PaginationTable';
 
 function App() {
   return (
     <div className="App">
-      <FilteringTable />
+      <PaginationTable />
+      {/* <FilteringTable /> */}
       {/* <SortingTable /> */}
       {/* <BasicTable /> */}
     </div>
