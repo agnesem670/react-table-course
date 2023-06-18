1fbe3a5e24888eaa35d2c0154b6b71f688cba316
agnesem670 <agnesem670@gmail.com>
2023-06-18 13:22:25 +0200

    RowSelection

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import BasicTable from './components/BasicTable';
 import SortingTable from './components/SortingTable';
 import FilteringTable from './components/FilteringTable';
 import PaginationTable from './components/PaginationTable';
+import RowSelection from './components/RowSelection';
 
 function App() {
   return (
     <div className="App">
-      <PaginationTable />
+      <RowSelection />
+      {/* <PaginationTable /> */}
       {/* <FilteringTable /> */}
       {/* <SortingTable /> */}
       {/* <BasicTable /> */}
---
+++ src/components/Checkbox.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Checkbox = React.forwardRef(({indeterminate, ...rest}, ref) => {
+    const deafultRef = React.useRef()
+    const resolvedRef = ref || deafultRef
+
+    React.useEffect(() => {
+        resolvedRef.current.indeterminate = indeterminate
+    }, [resolvedRef, indeterminate])
+
+  return (
+    <>
+        <input type='checkbox' ref={resolvedRef} {...rest} />
+    </>
+  )
+})
+
